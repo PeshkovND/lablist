5a15cfb59a786e4b5197dd43478c4321e9670d9b
Никита Пешков <peshkov.nikita4716@gmail.com>
2022-07-12 16:31:19 +0600

    feat: parse lab status into color

--- components/table/resultTable/tableContent/row/score/index.tsx
+++ components/table/resultTable/tableContent/row/score/index.tsx
@@ -2,15 +2,34 @@ import styles from "../../../../../../styles/score.module.css";
 import { DoneLab } from "../../../../../../types";
 
 interface ScoreProps {
-    elem: DoneLab;
-  }
+  elem: DoneLab;
+}
 
 export const Score = (props: ScoreProps) => {
-  return <div className={styles.scoreContainer}>
-    <div className={styles.score}>
-      <div className={styles.centerText}>
-        <p className={styles.scoreNumber}>{props.elem.score}</p>
+  const checkStatus = (elem: DoneLab) => {
+    let color: string;
+    switch (elem.status) {
+      case 3:
+        return;
+      case 0:
+        color = styles.done;
+        break;
+      case 1:
+        color = styles.send;
+        break;
+      default:
+        color = styles.return;
+    }
+    return (
+      <div className={styles.score + ' ' + color}>
+        <div className={styles.centerText}>
+          <p className={styles.scoreNumber}>{elem.score}</p>
+        </div>
       </div>
-    </div>
+    );
+  };
+
+  return <div className={styles.scoreContainer}>
+    {checkStatus(props.elem)}
   </div>;
 };
