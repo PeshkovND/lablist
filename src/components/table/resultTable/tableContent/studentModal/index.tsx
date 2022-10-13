2d43efe8bdf09dd103b2f06cb6800667874a6734
Nikita Peshkov <peshkov.nikita4716@gmail.com>
2022-10-13 11:56:40 +0600

    feat: lab statistic

--- src/components/table/resultTable/tableContent/studentModal/index.tsx
+++ src/components/table/resultTable/tableContent/studentModal/index.tsx
@@ -1,4 +1,4 @@
-import { Student } from "../../../../../types";
+import { DoneLab, Student } from "../../../../../types";
 import { ContactElem } from "./contactElem";
 import styles from "./studentModal.module.css";
 
@@ -11,6 +11,7 @@ interface ModalProps {
 
 export const StudentModal = (props: ModalProps) => {
 
+  const doneLabs: DoneLab[] = props.student ? props.student.done.filter((elem) => elem.status === 0) : []
   const setUnactive = () => {
     props.setActive(false)
     props.setStudent(null)
@@ -29,11 +30,25 @@ export const StudentModal = (props: ModalProps) => {
     if (student.contacts) {
       return (
         <div className={styles.anotherInfoContainer}>
-          {student.contacts.map(elem => {return <ContactElem key={elem.id} contact={elem}/>})}
+          {student.contacts.map(elem => { return <ContactElem key={elem.id} contact={elem} /> })}
         </div>
       )
     }
   }
+
+  const checkWork = (doneLabs: DoneLab[]) => {
+    const count = doneLabs.length
+    const rem = count % 10;
+    const secondRem = count % 100;
+    if (rem === 1 && secondRem !== 11) {
+      return <p className={styles.counter}>Выполнена <span>{count}</span> работа</p>
+    }
+    if (2 <= rem && rem <= 4 && ![12, 13, 14].includes(secondRem)) {
+      return <p className={styles.counter}>Выполнено <span>{count}</span> работы</p>
+    }
+    else return <p className={styles.counter}>Выполнено <span>{count}</span> работ</p>
+  }
+
 
   const checkStudent = (student: Student | null) => {
     if (student) {
@@ -78,6 +93,13 @@ export const StudentModal = (props: ModalProps) => {
             <div className={styles.infoContainer}>
               <p className={styles.title}>Сообщения</p>
             </div>
+          </div>
+          <div className={styles.compliteContainer}>
+            <div className={styles.statistic}>
+              <p className={styles.title}>Выполнение работ</p>
+              {checkWork(doneLabs)}
+            </div>
+            <div></div>
           </div>
         </div>
       )
